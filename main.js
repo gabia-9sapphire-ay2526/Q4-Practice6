24775384a20d5e4f4e22c5c8da70a48fedbd008e
gabia-9sapphire-ay2526 <lea.miller130@gmail.com>
2026-04-28 07:53:44 +0800

    For Loop

---
+++ main.js
@@ -0,0 +1,6 @@
+function countAnimals() {
+    var animals = ["Lion", "Flamingo","Polar ear", "Boa Constrictor"]
+    for (var i = 0; i < animals.length; i++) {
+        window.alert("This zoo containes a " + animals[i] + ".");
+    }
+}
